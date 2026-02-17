44b43ca062f8626ec5901c00ab5341c24ded529b
Karen Varela <karenperarnau@gmail.com>
2026-02-17 15:21:49 +0000

    style: add basic styles to JobList component

--- src/components/JobList/index.jsx
+++ src/components/JobList/index.jsx
@@ -21,26 +21,79 @@ const JobList = ({ jobs, onApply }) => {
   };
 
   return (
-    <div style={{ padding: "20px" }}>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        padding: "20px",
+      }}
+    >
       <h1>Nimble Gravity</h1>
-      <h2>Welcome to the Job Board</h2>
+      <h2>Welcome to the Job Board!</h2>
 
-      {jobs.map((job) => (
-        <div key={job.id} style={{ marginBottom: "20px" }}>
-          <h3>{job.title}</h3>
+      <div
+        style={{
+          display: "flex",
+          flexWrap: "wrap",
+          alignItems: "center",
+          justifyContent: "center",
+          gap: "20px",
+          marginTop: "20px",
+        }}
+      >
+        {jobs.map((job) => (
+          <div
+            key={job.id}
+            style={{
+              borderWidth: "2px",
+              borderStyle: "solid",
+              borderColor: "#3A9AFF",
+              borderRadius: "8px",
+              padding: "20px",
+              width: "100%",
+              maxWidth: "300px",
+              marginBottom: "20px",
+            }}
+          >
+            <h3>{job.title}</h3>
 
-          <label htmlFor={`repo-${job.id}`}>Enter your GitHub Repo URL:</label>
+            <label htmlFor={`repo-${job.id}`}>
+              Enter your GitHub Repo URL:
+            </label>
 
-          <input
-            id={`repo-${job.id}`}
-            type="text"
-            placeholder="GitHub repo URL"
-            onChange={(e) => onChangeInput(e, job.id)}
-          />
+            <input
+              id={`repo-${job.id}`}
+              type="text"
+              placeholder="GitHub repo URL"
+              style={{
+                marginTop: "5px",
+                width: "60%",
+                padding: "8px",
+                borderRadius: "4px",
+                border: "1px solid #ccc",
+              }}
+              onChange={(e) => onChangeInput(e, job.id)}
+            />
 
-          <button onClick={() => onSubmit(job.id)}>Submit</button>
-        </div>
-      ))}
+            <button
+              style={{
+                marginTop: "10px",
+                backgroundColor: "#261CC1",
+                color: "#fff",
+                border: "none",
+                padding: "5px 10px",
+                marginLeft: "10px",
+                borderRadius: "4px",
+                cursor: "pointer",
+              }}
+              onClick={() => onSubmit(job.id)}
+            >
+              Submit
+            </button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
